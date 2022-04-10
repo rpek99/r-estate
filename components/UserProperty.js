9348cc237a2f25f2a411077509c8f13545759245
rpek99 <rstmpk28@gmail.com>
2022-04-10 18:21:13 +0300

    user properties page created

---
+++ components/UserProperty.js
@@ -0,0 +1,49 @@
+import CheckIcon from '@mui/icons-material/Check';
+import RemoveCircleOutlineIcon from '@mui/icons-material/RemoveCircleOutline';
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
+import CloseIcon from '@mui/icons-material/Close';
+import { Card, CardContent, CardMedia, Divider, IconButton, Typography, Tooltip } from '@mui/material';
+
+
+const UserProperty = (props) => {
+
+    const { property } = props;
+
+    return (
+        <Card sx={{ display: 'flex', backgroundColor: "#e0e0e0", height: 200, marginTop: 5 }}>
+            <CardMedia
+                component="img"
+                sx={{ width: '30%'}}
+                image={property.image}
+            />
+            <CardContent sx={{ m : 2, marginTop: 3, marginLeft: 5, minWidth: 200 }}>
+                <Typography sx={{ fontSize: 20}}>P. Location</Typography>
+                <Divider sx={{ width: 100, marginTop: 1}}/>
+                <Typography sx={{ fontSize: 18, marginTop: 2}}>{property.location}</Typography>
+            </CardContent>
+            <CardContent sx={{ m : 2, marginLeft: 0, marginTop: 3 }}>
+                <Typography sx={{ fontSize: 20}}>P. Price</Typography>
+                <Divider sx={{ width: 90, marginTop: 1}}/>
+                <Typography sx={{ fontSize: 18, marginTop: 2}}>{property.price}$</Typography>
+            </CardContent>
+            <CardContent sx={{ m : 2, marginLeft: 0, marginTop: 3 }}>
+                <Typography sx={{ fontSize: 20}}>P. Number</Typography>
+                <Divider sx={{ width: 110, marginTop: 1}}/>
+                <Typography sx={{ fontSize: 18, marginTop: 2}}>#{property.number}</Typography>
+            </CardContent>
+            <CardContent sx={{ m : 2, marginLeft: 0, marginTop: 3 }}>
+                <Typography sx={{ fontSize: 18}}>On Sale</Typography>
+                <Divider sx={{ width: 80, marginTop: 1}}/>
+                {property.onSale ? <CheckIcon sx={{ marginTop: 2, marginLeft: 3}} /> : <CloseIcon sx={{ marginTop: 2, marginLeft: 3}}/>}
+                
+            </CardContent>
+            <CardContent sx={{ marginLeft: 5}}>
+                <IconButton>
+                    {property.onSale ? <Tooltip title="Remove From Sale"><RemoveCircleOutlineIcon /></Tooltip> : <Tooltip title="Put On Sale"><AddCircleOutlineIcon/></Tooltip>}
+                </IconButton>
+            </CardContent>
+        </Card>        
+    )
+}
+
+export default UserProperty;
--- pages/sale-properties.js
+++ pages/sale-properties.js
@@ -1,10 +1,51 @@
+import { Grid, Card, Container, Typography } from "@mui/material";
 import Navbar from "../components/Navbar";
+import UserProperty from "../components/UserProperty";
+import Footer from "../components/Footer";
+
+
+const properties = [
+    {
+        location: "İstanbul/Beylikdüzü",
+        price: "100.000",
+        number: "5032",
+        onSale: true,
+        image: "https://media.architecturaldigest.com/photos/56d9ef71ce4f38152ccc96f4/2:1/w_5130,h_2565,c_limit/designers-homes-02.jpg",
+    },
+    {
+        location: "İstanbul/Beşiktaş",
+        price: "300.000",
+        number: "5035",
+        onSale: false,
+        image: "https://media.architecturaldigest.com/photos/571e97c5741fcddb16b559c9/2:1/w_5127,h_2563,c_limit/modernist-decor-inspiration-01.jpg",
+    }
+]
+
 
 const SaleProperties = () => {
     return (
         <div>
             <Navbar />
-            <h1>User's Properties Page</h1>
+            <Card sx={{ marginTop: 12, display: 'flex', backgroundColor: '#f5f5f5', boxShadow: 'none', height: 80}}>
+                <Container maxWidth="lg">
+                    <Grid container>
+                            <Grid item sx={{ marginTop: 1 }} xs={3}>
+                                <Grid>
+                                    <Typography sx={{ fontFamily: 'Raleway', fontSize: 30, color: '#424242'}}>My Properties</Typography>
+                                </Grid>
+                            </Grid>
+                    </Grid>
+                </Container>
+            </Card>
+            <Container maxWidth="lg" sx={{ marginBottom: 10 }}>
+                {properties.map((property) => (
+                    <UserProperty property={property} />
+                ))}
+            </Container>
+            <Footer 
+                contactTitle="Contact Us" 
+                contactInfo="r_estate@gmail.com"
+            />
         </div>
     )
 }
